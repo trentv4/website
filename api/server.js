646afc73bdda937c6aa4396a1957c4ee63c2fc83
Trent VanSlyke <trentvanslyke@gmail.com>
2016-01-11 11:57:06 -0500

    Fixing server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+
 app.get('/api/watch', function (req, res) {
   res.end("fuck off");
-  console.log("Request handled.")
 })
+
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
