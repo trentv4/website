ac825b6bbbdda90137f39d70768c3d8b93316bfe
Trent VanSlyke <trentvanslyke@gmail.com>
2016-01-11 16:53:06 +0000

    hack webms mostly

---
+++ api/server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get('/api/watch', function (req, res) {
+  res.end("fuck off");
+  console.log("Request handled.")
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
