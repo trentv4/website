9e3112f150e5c1d2f3a968609baf382ec646e717
trentv4 <trentvanslyke@gmail.com>
2016-02-11 19:48:52 -0500

    jesus node y won't you let express install

--- api/server.js
+++ api/server.js
@@ -3,20 +3,20 @@ var express = require('express');
 var app = express()
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.json({message: 'horray'});
+///////////////////
+router.route('/test/')
+.post(function(req, res){
+  console.log(req);
 })
+.get(function(req, res){
 
-router.route('/').post(function(req, res){
-  console.log(res);
 })
+///////////////////
+
 app.use('/api', router);
-
 app.listen(port);
 console.log("running");
