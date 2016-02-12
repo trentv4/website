c9c6e56de71d03b869ed5fd510906c39bf05b0cd
trentv4 <trentvanslyke@gmail.com>
2016-02-11 20:31:41 -0500

    Remove webm index, got api/ to work relatively well

--- api/server.js
+++ api/server.js
@@ -1,22 +1,17 @@
 var http = require('http');
 var express = require('express');
 var app = express()
-var bodyParser = require('body-parser')
-
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
 ///////////////////
-router.route('/test/')
-.post(function(req, res){
-  console.log(req);
-})
+router.route('/navyseal/')
 .get(function(req, res){
-
+	console.log("Navy Seal request")
+	res.status(404).end();
 })
 ///////////////////
-
 app.use('/api', router);
 app.listen(port);
 console.log("running");
