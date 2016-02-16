6f7113b3b79c8474dd3909b3c15fecde025c9c3f
trentv4 <trentvanslyke@gmail.com>
2016-02-16 06:32:46 -0500

    Add 2 new navy seals, update server module a bit, add basics to my portfolio.

--- api/server.js
+++ api/server.js
@@ -2,15 +2,20 @@ var http = require('http');
 var express = require('express');
 var app = express()
 
-var port = 80;
+var port = 3000;
 var router = express.Router();
+
+///////////////////
+
 
 ///////////////////
 router.route('/navyseal/')
 .get(function(req, res){
-	res.status(404).end();
+	console.log("Navy seal request obtained.");
+	res.send();
+	res.end();
 })
-////////////////////
+///////////////////
 app.use('/api/', router);
 app.listen(port);
 console.log("NodeJS Backend API running.");
