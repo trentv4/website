b4db5534504884dd0cdc76c57881eb9779a18077
Trent VanSlyke <trentvanslyke@gmail.com>
2016-05-13 18:37:46 -0400

    Update bf.js to a rewritten module

--- brainfuck/brainfuck.js
+++ brainfuck/brainfuck.js
@@ -1,75 +1,66 @@
-var halt = false;
-var autohalt = false;
-var commands_s;
-var commands = []
-var c_index = 0;
-var memory = [0]
-var m_index = 0;
-var loops = []
+var interval_delay;
+/*
+>	Move the pointer to the right
+<	Move the pointer to the left
++	Increment the memory cell under the pointer
+-	Decrement the memory cell under the pointer
+.	Output the character signified by the cell at the pointer
+,	Input a character and store it in the cell at the pointer
+[	Jump past the matching ] if the cell under the pointer is 0
+]	Jump back to the matching [ if the cell under the pointer is nonzero
+*/
 
-var loop_hardcap = 1000;
-var loop_cap_counter = 0;
+var command_list = {
+    ">": function() { if(memory.length > memory.index) { memory.index++ } else { memory.index++; memory.list.push(0) }},
+    "<": function() { if(memory.index > 0) { memory.index-- } },
+    "+": function() { memory.list[memory.index]++ },
+    "-": function() { if(memory.list[memory.index] > 0) { memory.list[memory.index]-- } },
+    ".": function() { document.getElementById("output").value += memory.list[memory.index] + " " },
+    ",": function() { memory.list[memory.index] = parseInt(prompt("Enter a number")) },
+    "[": function() {  },
+    "]": function() {  }
+}
 
-var add = function() { memory[m_index]++ }
-var sub = function() { if(memory[m_index] > 0) { memory[m_index]--; } }
-var shiftleft = function() { if(m_index > 0) {m_index--} }
-var shiftright = function() { if(memory.length >= m_index){memory.push(0)} m_index++ }
-var getinput = function() { }
-var print = function() { document.getElementById("output").value += memory[m_index] + " ";}
-var loopstart = function() { loops.push(c_index+1) }
+var commands = {
+    index: 0,
+    list: [],
+}
 
-var loopend = function() {
-	if(memory[m_index] != 0 & loop_hardcap > loop_cap_counter) {
-		c_index = loops[loops.length-1]-1
-		loop_cap_counter++;
-	}
-	else {
-		loop_cap_counter = 0;
-		loops.pop()
-	}
+var memory = {
+    index: 0,
+    list: [0],
+}
+
+function bf_load() {
+    var data = document.getElementById("input").value;
+    console.log("Loading: " + data)
+    var i = data.split("")
+    for(var g = 0; g < i.length; g++)
+    {
+        commands.list.push(command_list[i[g]])
+    }
+    commands.list.push("NOP")
+}
+
+function bf_step() {
+    commands.list[commands.index]()
+    commands.index++;
 }
 
 function bf_stop() {
-	autohalt = true;
+    clearInterval(interval_delay)
 }
 
-function bf_load_and_exec() {
-	console.log(document.getElementById("output").value)
-	document.getElementById("output").value = "";
-	bf_parse(document.getElementById("input").value)
-	bf_exec()
+function bf_clear() {
+    commands = {
+        index: 0,
+        list: [],
+    }
 }
 
-var bf_exec = function() {
-	console.log("excecuting")
-	halt = false
-	while(commands[c_index] != "end" & halt == false) {
-		bf_tick()
-		if(autohalt) {
-			halt = true
-		}
-	}
+function bf_run() {
+    while(commands.list[commands.index] != "NOP")
+    {
+        bf_step()
+    }
 }
-
-var bf_tick = function() {
-	commands[c_index]();
-	c_index++;
-}
-
-var bf_parse = function(input) {
-	commands_s = input.split('')
-	for(var i = 0; i < commands_s.length; i++)
-	{
-		if(commands_s[i] == '+') { commands.push(add) }
-		if(commands_s[i] == '-') { commands.push(sub) }
-		if(commands_s[i] == '.') { commands.push(print) }
-		if(commands_s[i] == ',') { commands.push(getinput) }
-		if(commands_s[i] == '>') { commands.push(shiftright) }
-		if(commands_s[i] == '<') { commands.push(shiftleft) }
-		if(commands_s[i] == '[') { commands.push(loopstart) }
-		if(commands_s[i] == ']') { commands.push(loopend) }
-	}
-	commands.push("end")
-}
-//bf_parse("+++++[->+++++<]>.")
-//bf_exec()
