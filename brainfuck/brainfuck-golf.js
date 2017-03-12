f83ac21d93180e0dcaa77fc2dc690ac034d471bd
trentv4 <trentvanslyke@gmail.com>
2017-03-12 03:53:29 -0400

    [BRAINFUCK] I made the golf worse, because javascript

--- brainfuck/brainfuck-golf.js
+++ brainfuck/brainfuck-golf.js
@@ -1,35 +1,26 @@
-/*
->	Move the pointer to the right
-<	Move the pointer to the left
-+	Increment the memory cell under the pointer
--	Decrement the memory cell under the pointer
-.	Output the character signified by the cell at the pointer
-,	o a character and store it in the cell at the pointer
-[	Jump past the matching ] if the cell under the pointer is 0
-]	Jump back to the matching [ if the cell under the pointer is nonzero
-*/
-
-var o = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
-var c = []
-var q = 0;
-var d = []
-var s = 0
-var j = ""
-for(var i = 0; i < o.length; i++)
-{
-	if(o[i] == "<") c.push(function(){if(s > 0) s--})
-	if(o[i] == ">") c.push(function(){s++})
-	if(o[i] == "+") c.push(function(){if(d[s] == null) d[s] = 0; d[s]++})
-	if(o[i] == "-") c.push(function(){if(d[s] == null) d[s] = 0; d[s]--})
-	if(o[i] == ".") c.push(function(){j += String.fromCharCode(d[s])})
-	if(o[i] == ",") c.push(function(){d[s] = parseInt(prompt("Enter a number"))})
-	if(o[i] == "[") c.push(function()
+o = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+."
+u = o.length
+c = []
+c.z = c.push
+q = 0;
+d = []
+s = 0
+n = null
+j = ""
+e = {
+	"<":function(){if(s > 0) s--},
+	">":function(){s++},
+	"+":function(){if(d[s] == n) d[s] = 0; d[s]++},
+	"-":function(){if(d[s] == n) d[s] = 0; d[s]--},
+	".":function(){j += String.fromCharCode(d[s])},
+	",":function(){d[s] = parseInt(prompt("Enter a number"))},
+	"[":function()
 	{
-		if(d[s] == undefined) d[s] = 0;
+		if(d[s] == n) d[s] = 0;
 		if(d[s] == 0)
 		{
 			var f = 0
-			for(var i = q; i < o.length; i++)
+			for(var i = q; i < u; i++)
 			{
 				if(o[i] == "[") f++;
 				if(o[i] == "]" & f != 0) f--;
@@ -40,10 +31,10 @@ for(var i = 0; i < o.length; i++)
 				}
 			}
 		}
-	})
-	if(o[i] == "]") c.push(function()
+	},
+	"]":function()
 	{
-		if(d[s] == undefined) d[s] = 0;
+		if(d[s] == n) d[s] = 0;
 		if(d[s] != 0)
 		{
 			var f = 0
@@ -58,9 +49,13 @@ for(var i = 0; i < o.length; i++)
 				}
 			}
 		}
-	})
+	}
 }
-for(q = 0; q < c.length; q++)
+for(i in o)
+{
+	c.z(e[o[i]])
+}
+for(q = 0; q < u; q++)
 {
 	c[q]();
 }
