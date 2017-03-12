d51f97498870349eeb6c82e0ad27859a834946c9
trentv4 <trentvanslyke@gmail.com>
2017-03-12 15:27:43 -0400

    [BRAINFUCK] Golf formatting

--- brainfuck/brainfuck-golf.js
+++ brainfuck/brainfuck-golf.js
@@ -2,10 +2,8 @@ o = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->+++..
 d = []
 s = 0
 j = ""
-function a(b)
-{
-	for (f = b; f;)
-	{
+function a(b) {
+	for (f = b; f;) {
 		if(o[q += b] == "[") f++
 		if(o[q] == "]") f--
 	}
@@ -17,14 +15,8 @@ e = {
 	"-":function(){d[s] = (d[s] || 0) - 1},
 	".":function(){j += String.fromCharCode(d[s])},
 	",":function(){d[s] = parseInt(prompt("Enter a number"))},
-	"[":function()
-	{
-		if(!d[s]) a(1)
-	},
-	"]":function()
-	{
-		if(d[s]) a(-1)
-	}
+	"[":function(){if(!d[s]) a(1)},
+	"]":function(){if(d[s]) a(-1)}
 }
 for(q = 0; q < o.length; q++) e[o[q]]()
 console.log(j, d)
