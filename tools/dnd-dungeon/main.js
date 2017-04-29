2ce7e7a07604870b9498b27f20afdd4481a68474
trentv4 <trentvanslyke@gmail.com>
2017-04-29 04:40:48 -0400

    Implement get/add/remove with "all" support for the map object

--- tools/dnd-dungeon/main.js
+++ tools/dnd-dungeon/main.js
@@ -14,36 +14,79 @@ let display = {
 let map = {
   data: [[]],
   verifyArrayFits: (x, y) => {
+    let data = map.data
+
     if(data.length < x) {
-      let missingDataLength = x - data.length
+      let missingDataLength = x - data.length + 1
       for(let i = 0; i < missingDataLength; i++) {
         data.push([])
+      }
+    }
+    if(data[x].length < y) {
+      let missingDataLength = y - data[x].length + 1
+      for(let i = 0; i < missingDataLength; i++) {
+        data[x].push([])
       }
     }
   },
   get: (type, x, y) => {
     let data = map.data
+    map.verifyArrayFits(x, y)
 
-    return data[x - 1][y - 1]
+    if(type == "all") return data[x][y]
+
+    let position = data[x][y]
+    for(let i = 0; i < position.length; i++)
+    {
+      if(position[i] != null)
+      {
+        if(position[i].type == type) return position[i]
+      }
+    }
   },
   add: (type, x, y, rotation) => {
     let data = map.data
+    map.verifyArrayFits(x, y)
+
+    if(map.get(type, x, y) != null) return
 
     if(rotation == null) rotation = 0
+
     let newObject = {
       x: x,
       y: y,
-      type: currentType,
+      type: type,
       rotation: rotation
     }
 
-    console.log(data[x - 1][y - 1])
+    data[x][y].push(newObject)
   },
   remove: (type, x, y, rotation) => {
+    let data = map.data
+    map.verifyArrayFits(x, y)
 
+    if(map.get(type, x, y) == null) return
+
+    let position = data[x][y]
+
+    if(type == "all")
+    {
+      data[x][y] = []
+      return
+    }
+
+    for(let i = 0; i < position.length; i++)
+    {
+      if(position[i].type == type)
+      {
+        data[x][y][i] = null
+        return
+      }
+    }
   }
 }
 
 map.add("asdf", 5, 5)
-map.get(null, 5, 5)
-console.log(map.data)
+map.remove("asdf", 5, 5)
+map.add("asdf", 5, 5)
+console.log(map.get("asdf", 5, 5))
