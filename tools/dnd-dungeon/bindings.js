1b0c7dde30d6d82652df50fc3ee5ac5d236d68b3
trentv4 <trentvanslyke@gmail.com>
2017-05-06 22:26:56 -0400

    [DND-DUNGEON] Add message command to interact with alerts below the IO center

--- tools/dnd-dungeon/bindings.js
+++ tools/dnd-dungeon/bindings.js
@@ -1,4 +1,11 @@
 window.get = (e) => document.getElementById(e)
+window.message = (color, str) => {
+  get("message").innerHTML = "<br>"
+  setTimeout(() => {
+    get("message").style = "color: " + color
+    get("message").innerHTML = str
+  }, 500)
+}
 
 let mouse = {
 	x: 0,
