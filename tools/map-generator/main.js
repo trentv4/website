e286aa657e947921bc0acbd0ee4ff5b11eb4ba04
trentv4 <trentvanslyke@gmail.com>
2017-06-06 14:19:47 -0400

    [MAPGEN] Modularize the display a bit, add blank registration to webgl rendering

--- tools/map-generator/main.js
+++ tools/map-generator/main.js
@@ -1,15 +1,25 @@
+perlin = (x, y, frequency, amplitude) => noise.perlin2(x * frequency, y *  frequency) * amplitude
+simplex = (x, y, frequency, amplitude) => noise.simplex2(x * frequency, y *  frequency) * amplitude
+color = (r,g,b) => ({r: r, g: g, b: b})
+greyscale = (i) => ({r: i*255, g: i*255, b: i*255})
+
 let display = {
-  register: (id, x, y, drawFunc) => {
+  register: (id, x, y, drawFunc, context, z) => {
     let object = document.createElement("canvas")
     object.id = id
     object.width = x
     object.height = y
+    object.style = "z-index: " + z
+    object.setZIndex = (e) => {
+      object.style = "z-index: " + e
+    }
     document.getElementById("page").appendChild(object)
-    let ctx = object.getContext("2d")
+    let ctx = object.getContext(context)
     ctx.width = x
     ctx.height = y
     display[id] = {
       canvas: ctx,
+      element: object,
       draw: drawFunc
     }
   }
@@ -21,18 +31,38 @@ let mapY = 800
 let mapData = {
   data: [],
   populate: () => {
-    for(let nx = 0; nx < mapX; nx++) {
-      if(mapData.data[nx] == null) mapData.data[nx] = []
-      for(let ny = 0; ny < mapY; ny++) {
-        if(mapData.data[nx][ny] == null) mapData.data[nx][ny] = {r: 128, g: 0, b: 128}
-        mapData.data[nx][ny] = getPixel(nx, ny, mapData.data[nx][ny])
+    for(let x = 0; x < mapX; x++) {
+      if(mapData.data[x] == null) mapData.data[x] = []
+      for(let y = 0; y < mapY; y++) {
+        if(mapData.data[x][y] == null) mapData.data[x][y] = {r: 128, g: 0, b: 128}
+
+        let nx = x / mapX - 0.5
+        let ny = y / mapY - 0.5
+
+        let pixel = 0
+        pixel += perlin(nx, ny, 4, 0.5)
+        pixel += perlin(nx, ny, 8, 0.25)
+        pixel += perlin(nx, ny, 16, 0.125)
+        pixel += perlin(nx, ny, 32, 0.125)
+
+        pixel = pixel + 1 / 2
+
+        pixel = Math.pow(pixel, 2.0)
+
+        let c = greyscale(pixel)
+        if(pixel < 0.6) c.g += 64
+        if(pixel < 0.2) c.b += 128
+
+        mapData.data[x][y] = c
       }
     }
   }
 }
 
-display.register("main", mapX, mapY, (mapData) => {
-  let c = display.main.canvas
+/* This is the 2D heightmap version */
+
+display.register("terrain2D", mapX, mapY, (mapData) => {
+  let c = display.terrain2D.canvas
 
   let imageData = c.getImageData(0, 0, mapX, mapY)
   for(let nx = 0; nx < mapX; nx++) {
@@ -46,38 +76,16 @@ display.register("main", mapX, mapY, (mapData) => {
   }
 
   c.putImageData(imageData, 0, 0)
-})
+}, "2d", 0)
 
-// Actual generation goes here
+/* This is the 3D heightmap / terrain view version */
 
-noise.seed(Math.random())
+display.register("terrain3D", mapX, mapY, (mapData) => {
+  let c = display.terrain3D.canvas
 
-n = (x, y, frequency, amplitude) => noise.perlin2(x * frequency, y *  frequency) * amplitude
+  
+}, "webgl", 0)
 
-function getPixel(x, y, initial) {
-  let nx = x / mapX - 0.5
-  let ny = y / mapY - 0.5
-
-  let pixel = 0
-  pixel += n(nx, ny, 4, 0.5)
-  pixel += n(nx, ny, 8, 0.25)
-  pixel += n(nx, ny, 16, 0.125)
-  pixel += n(nx, ny, 32, 0.125)
-
-  pixel = pixel + 1 / 2
-
-  pixel = Math.pow(pixel, 2.0)
-
-  let c = greyscale(pixel)
-  if(pixel < 0.6) c.g += 64
-  if(pixel < 0.2) c.b += 128
-
-//  c = greyscale(pixel)
-  return c
-}
-
-color = (r,g,b) => ({r: r, g: g, b: b})
-greyscale = (i) => ({r: i*255, g: i*255, b: i*255})
-
+noise.seed(Math.random()*0)
 mapData.populate()
-display.main.draw(mapData)
+display.terrain3D.draw(mapData)
