6a705b0ff4418002ebf67c14911bff27ca6d4f58
trentv4 <trentvanslyke@gmail.com>
2017-06-06 05:37:28 -0400

    [MAPGEN] Finally fixed rendering issue. Begin work proper - add simplex noise

--- tools/map-generator/main.js
+++ tools/map-generator/main.js
@@ -15,14 +15,16 @@ let display = {
   }
 }
 
-let mapX = 10
-let mapY = 20
+let mapX = 600
+let mapY = 800
 
 let mapData = {
-  data: new Array(mapX).fill(new Array(mapY).fill({r: 128, g: 0, b: 128})),
+  data: [],
   populate: () => {
     for(let nx = 0; nx < mapX; nx++) {
+      if(mapData.data[nx] == null) mapData.data[nx] = []
       for(let ny = 0; ny < mapY; ny++) {
+        if(mapData.data[nx][ny] == null) mapData.data[nx][ny] = {r: 128, g: 0, b: 128}
         mapData.data[nx][ny] = getPixel(nx, ny, mapData.data[nx][ny])
       }
     }
@@ -36,11 +38,9 @@ display.register("main", mapX, mapY, (mapData) => {
   for(let nx = 0; nx < mapX; nx++) {
     for(let ny = 0; ny < mapY; ny++) {
       let i = (ny * mapX + nx) * 4 - 1
-      let d = mapData.data[nx][ny]
-
-      imageData.data[++i] = d.r
-      imageData.data[++i] = d.g
-      imageData.data[++i] = d.b
+      imageData.data[++i] = mapData.data[nx][ny].r
+      imageData.data[++i] = mapData.data[nx][ny].g
+      imageData.data[++i] = mapData.data[nx][ny].b
       imageData.data[++i] = 255
     }
   }
@@ -54,21 +54,8 @@ noise.seed(0)
 
 noise1 = (x, y) => noise.simplex2(x,y)/2 + 0.5
 
-function getPixel(xIn, yIn, initial) {
-  let newColor = {
-    r: 0,
-    g: 0,
-    b: 0
-  }
-
-  if(yIn % 2 == 0) {
-    newColor.r = 255
-  }
-  if(xIn % 2 == 0) {
-    newColor.g = 255
-  }
-
-  return newColor
+function getPixel(x, y, initial) {
+  return greyscale(noise1(x, y))
 }
 
 greyscale = (i) => {
