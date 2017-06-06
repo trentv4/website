2d8e7300477d05403f5e7c9398f5b0e618a6b2f5
trentv4 <trentvanslyke@gmail.com>
2017-06-06 02:42:20 -0400

    [MAPGEN] Clean up and simplify framework, fix interaction between rendering -> map

--- tools/map-generator/main.js
+++ tools/map-generator/main.js
@@ -1,54 +1,22 @@
-get = (e) => document.getElementById(e)
-make = (e) => document.createElement(e)
-
 let display = {
-  colors: {
-    background: "#FFFFFF"
-  },
-  index: 0,
   register: (id, x, y, drawFunc) => {
-    let object = make("canvas")
+    let object = document.createElement("canvas")
     object.id = id
     object.width = x
     object.height = y
-    object.style = "z-index: " + display.index
-    display.index++
-    get("canvas-container").appendChild(object)
-    if(display[id] == null) display[id] = {}
-    display[id].canvas = object.getContext("2d")
-    if(drawFunc != null) display[id].draw = drawFunc
-  },
-  draw: () => {
-    display.background.draw(mapData)
+    document.getElementById("page").appendChild(object)
+    display[id] = {
+      canvas: object.getContext("2d"),
+      draw: drawFunc
+    }
   }
 }
 
-let mapX = 600,
-    mapY = 800
-
-display.register("background", mapX, mapY, (mapData) => {
-  let c = display.background.canvas
-  c.fillStyle = display.colors.background
-  c.fillRect(0, 0, c.canvas.width, c.canvas.height)
-
-  let imageData = c.getImageData(0, 0, mapX, mapY)
-  let dat = imageData.data
-
-  for(let i = 0; i < dat.length; i += 4) {
-    let f = (i / dat.length) * 255
-    if(mapData.get(i) == null) console.log(i)
-    dat[i]     = mapData.get(i).r
-    dat[i + 1] = mapData.get(i).g
-    dat[i + 2] = mapData.get(i).b
-    dat[i + 3] = 255
-  }
-
-  c.putImageData(imageData, 0, 0)
-})
+let mapX = 800
+let mapY = 800
 
 let mapData = {
   data: new Array(mapX).fill(new Array(mapY).fill({r: 128, g: 0, b: 128})),
-  get: (i) =>  mapData.data[i % mapX][i % mapY],
   populate: () => {
     for(let x = 0; x < mapX; x++) {
       for(let y = 0; y < mapY; y++) {
@@ -58,14 +26,35 @@ let mapData = {
   }
 }
 
+display.register("main", mapX, mapY, () => {
+  let c = display.main.canvas
+
+  let imageData = c.getImageData(0, 0, mapX, mapY)
+
+  let i = 0;
+  for(let x = 0; x < mapX; x++) {
+    for(let y = 0; y < mapY; y++) {
+      i = ((x * mapX) + y) * 4
+      imageData.data[i]     = mapData.data[x][y].r
+      imageData.data[i + 1] = mapData.data[x][y].g
+      imageData.data[i + 2] = mapData.data[x][y].b
+      imageData.data[i + 3] = 255
+    }
+  }
+
+  c.putImageData(imageData, 0, 0)
+})
+
+// Actual generation goes here
+
 function getPixel(x, y, initial) {
   let pixel = {
     r: 0,
     g: 0,
     b: 0
   }
-  return initial
+  return pixel
 }
 
 mapData.populate()
-display.draw()
+display.main.draw()
