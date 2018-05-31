63bf0e0836b4121e1d245a4974901e4b3f4fc752
Trent VanSlyke <trentvanslyke@gmail.com>
2018-05-31 05:14:24 -0400

    Final site cleanup before final redeployment. Convert all of site to new .htmljs

--- routes/global.js
+++ routes/global.js
@@ -3,9 +3,7 @@ const fs = require("fs")
 let router = express.Router()
 
 router.get("*", (req, res) => {
-	let url = req.originalUrl.substring(1)
-	if(fs.existsSync(url + ".htmljs")) url += ".htmljs"
-	res.render(url, {req: req, res: res})
+	res.render(req.originalUrl.substring(1), {req: req, res: res})
 })
 
 module.exports = router
