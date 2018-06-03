ad69bbaed0a70761bd8b679b50d0a4c557ae0571
Trent VanSlyke <trentvanslyke@gmail.com>
2018-06-03 12:09:53 -0400

    Fix navyseal using old non-Promise async query

--- routes/navyseal.js
+++ routes/navyseal.js
@@ -3,7 +3,7 @@ let router = express.Router()
 let sql = require("../global/sql.js")
 
 router.get("/", (req, res) => {
-	sql.query("select title,content from navyseal", (err, rows, fields) => {
+	sql.query("select title,content from navyseal").then(rows => {
 		res.send(rows)
 	})
 })
