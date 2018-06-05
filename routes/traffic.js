909f833dbab58cf0254cc3f5b7dd30961535ddb6
Trent VanSlyke <trentvanslyke@gmail.com>
2018-06-05 04:39:37 -0400

    Split traffic tables into separate sub-tables for each state

--- routes/traffic.js
+++ routes/traffic.js
@@ -22,37 +22,49 @@ router.post("/forbid", (req, res) => {
 	if(!isValidUser(req.body.username, req.body.password)) return
 	let page = sql.mysql.escape(unescape(req.body.page))
 
-	sql.query("insert into forbidden (page) values ("+ page +")")
-	sql.query("update traffic set state='forbidden' where page="+ page)
-	console.write("... forbidding" + page)
-
+	sql.query("insert into forbidden (page) values ("+ page +")").then(rows => {
+		sql.query("update traffic set state='forbidden' where page="+ page).then(rows => {
+			console.write("... forbidding " + page)
+			res.status("200")
+			res.send("Forbid " + page)
+			res.end()
+		})
+	})
 })
 
 router.post("/delete", (req, res) => {
 	if(!isValidUser(req.body.username, req.body.password)) return
 	let page = sql.mysql.escape(unescape(req.body.page))
 
-	sql.query("delete from traffic where page="+ page)
-	console.write("... deleting " + page)
+	sql.query("delete from traffic where page="+ page).then(rows => {
+		console.write("... deleting " + page)
+		res.status("200")
+		res.send("Deleted " + page)
+		res.end()
+	})
 })
 
 router.post("/allow", (req, res) => {
 	if(!isValidUser(req.body.username, req.body.password)) return
 	let page = sql.mysql.escape(unescape(req.body.page))
 
-	sql.query("delete from forbidden where page="+ page)
-
-	let url = "." + page.substring(1, page.length-1)
-	if(fs.existsSync(url)) {
-		if(fs.statSync(url).isFile() && url != "./") {
-			sql.query("update traffic set state='file' where page="+ page)
-		} else {
-			sql.query("update traffic set state='valid' where page="+ page)
+	sql.query("delete from forbidden where page="+ page).then(rows => {
+		let url = "." + page.substring(1, page.length-1)
+		let state = "valid"
+		if(fs.existsSync(url) && fs.statSync(url).isFile()) {
+			state = "file"
+		} else if(url != "./") {
+			state = "missing"
 		}
-	} else {
-		sql.query("update traffic set state='missing' where page="+ page)
-	}
-	console.write("... allowing" + page)
+		console.log(state)
+		sql.query("update traffic set state='"+ state +"' where page="+ page).then(rows => {
+			console.write("... allowing " + page)
+			res.status("200")
+			res.send("Allowed " + page)
+			res.end()
+		})
+	})
+
 })
 
 module.exports = router
--- traffic/main.js
+++ traffic/main.js
@@ -4,6 +4,12 @@ function send(action, pageUrl) {
 	let request = new XMLHttpRequest()
 	request.open("POST", action)
 	// forbid delete allow
+	let count = 0
+	request.onreadystatechange = () => {
+		if(request.readyState == 4 && request.status == 200) {
+			document.location = document.location + ""
+		}
+	}
 
 	request.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
 	let data = {
@@ -11,7 +17,6 @@ function send(action, pageUrl) {
 		username: ""+document.getElementById("un").value, 
 		password: ""+document.getElementById("pw").value
 	}
-	console.log(JSON.stringify(data))
 	request.send(JSON.stringify(data))
 
 	request = new XMLHttpRequest()
@@ -21,3 +26,8 @@ function send(action, pageUrl) {
 
 document.getElementById("un").value = localStorage.getItem("un")
 document.getElementById("pw").value = localStorage.getItem("pw")
+window.scrollTo(0, localStorage.getItem("scrollPosition"))
+
+window.addEventListener("scroll", () => {
+	localStorage.setItem("scrollPosition", window.scrollY)
+})
